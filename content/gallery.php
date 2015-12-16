7a1c90e5e4a845a58d4f474ce4f94d29c64f8c6e
Vasy <007vasy@gmail.com>
2015-12-17 00:20:41 +0100

    gallery set

--- content/gallery.php
+++ content/gallery.php
@@ -3,22 +3,41 @@
 </h2>
 <center>
 	<?php
+		if ($handle = opendir('image')) {
+    		while (false !== ($entry = readdir($handle)))
+			{
+		        if ($entry != "." && $entry != "..")
+				{
+
+
+		            $kepek[] = $entry;
+
+
+
+		        }
+
+			}
+		    closedir($handle);
+			}
+
 		error_reporting(E_ALL ^ E_NOTICE);
 
 		if(empty($_POST["y"]))
 		$y=0;
-
+		$max=count($kepek);
 		$kep = $_GET["y"];
-
+		//echo($max);
 		$filename = 'image/'.$kep.'.jpg';
-		if (empty($kep)||$kep<"0")
-			$kep="0";
+		if (empty($kep)||$kep>$max-1)
+			$kep=$max-1;
+		if ($kep<"1")
+			$kep="1";
 
-		echo ('<a href="?np='.$_GET["np"].'&y='.($kep-1).'"> <<< </a>
+		echo ('<a href="?np='.$_GET["np"].'&y='.($kep-1).'"> Old </a>
 
 		&nbsp;&nbsp;&nbsp;&nbsp;
 
-		<a href="?np='.$_GET["np"].'&y='.($kep+1).'"> >>> </a><hr />');
+		<a href="?np='.$_GET["np"].'&y='.($kep+1).'"> New </a><hr />');
 
 
 
@@ -26,22 +45,7 @@
 
 
 
-	if ($handle = opendir('image')) {
-    while (false !== ($entry = readdir($handle)))
-	{
-        if ($entry != "." && $entry != "..")
-		{
-
-
-            $kepek[] = $entry;
-
-
-
-        }
-
-	}
-    closedir($handle);
-	}
+	
 
 echo (' <a class="img" href="image/'.$kepek[$kep].'" target="_blank"> <img id="img" src="image/'.$kepek[$kep].'"  width="320"/> </a>');
 
